d9927c9922d42ed3cff108cba05a266138839855
KeoniHan2024 <han24@up.edu>
2022-11-30 21:34:35 -0800

    Update Spotify.py
    Pushing average not duration and

--- Spotify.py
+++ Spotify.py
@@ -3,10 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as py
 import librosa 
 import music21
-from music21 import converter, corpus, instrument, midi, note, chord, pitch
+from music21 import converter, corpus, instrument, midi, note, chord, pitch, features
 
-#test
-file = converter.parse('./smallSet/ACDC.Highway_to_Hell_K.mid')
+# components = []
+# file = converter.parse('./smallSet/ACDC.Highway_to_Hell_K.mid')
+# for element in file.recurse():
+#     components.append(element)
+#     print(element)
 
 def openMidi(midi_path, remove_drums):
     mf = midi.MidiFile()
@@ -18,15 +21,23 @@ def openMidi(midi_path, remove_drums):
             mf.tracks[i].events = [ev for ev in mf.tracks[i].events if ev.channel != 10]
     return midi.translate.midiFileToStream(mf)
 
+def list_instruments(midi):
+    partStream = midiFile.parts.stream()
+    print("List of instruments found")
+    for p in partStream:
+        aux = p
+        print(p.partName)
 
-midiFile = openMidi('./smallSet/ACDC.Highway_to_Hell_K.mid', True)
-components = []
-for element in file.recurse():
-    components.append(element)
-    print(element)
+midiFile = openMidi('./smallSet/ACDC.Highway_to_Hell_K.mid', False)
+list_instruments(midiFile)
 
-partStream = midiFile.parts.stream()
-print("List of instruments found")
-for p in partStream:
-    aux = p
-print(p.partName)
+fileParsed = converter.parse('./smallSet/ACDC.Highway_to_Hell_K.mid')
+feature = features.jSymbolic.AverageNoteDurationFeature(fileParsed)
+f = feature.extract()
+print('Average Note Duration')
+print(f.vector)
+
+feature = features.jSymbolic.InitialTempoFeature(fileParsed)
+f = feature.extract()
+print('Initial Tempo')
+print(f.vector)
