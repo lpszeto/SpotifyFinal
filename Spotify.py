99da9ec322a7d74016b452eda49abca08b95080e
KeoniHan2024 <han24@up.edu>
2022-12-05 17:30:38 -0800

    Other stuff

--- Spotify.py
+++ Spotify.py
@@ -4,34 +4,11 @@ import pandas as pd
 import matplotlib.pyplot as py
 import librosa 
 import music21
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 import os
 from music21 import converter, corpus, instrument, midi, note, chord, pitch, features
 
-# components = []
-# file = converter.parse('./smallSet/ACDC.Highway_to_Hell_K.mid')
-# for element in file.recurse():
-#     components.append(element)
-#     print(element)
-
-# def openMidi(midi_path, remove_drums):
-#     mf = midi.MidiFile()
-#     mf.open(midi_path)
-#     mf.read()
-#     mf.close()
-#     if (remove_drums):
-#         for i in range(len(mf.tracks)):
-#             mf.tracks[i].events = [ev for ev in mf.tracks[i].events if ev.channel != 10]
-#     return midi.translate.midiFileToStream(mf)
-
-# def list_instruments(midi):
-#     partStream = midiFile.parts.stream()
-#     print("List of instruments found")
-#     for p in partStream:
-#         aux = p
-#         print(p.partName)
-
-# midiFile = openMidi('./smallSet/ACDC.Highway_to_Hell_K.mid', False)
-# list_instruments(midiFile)
 file = open('data.csv', 'w')
 writer = csv.writer(file)
 
@@ -55,4 +32,8 @@ for song in os.listdir('./smallSet'):
     writer.writerow(data)
 
 file.close()
-#PCA
+
+
+## GETTING SPOTIFY SONG DATA FROM USER
+
+
